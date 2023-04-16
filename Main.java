c0927d8093f0863e00e5a51c2cd0b0669653662e
Luke-Blair <lblair@mail.umw.edu>
2023-04-16 14:49:34 -0400

    Added comments to main

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-
+        //need to set up GUI
+            //maybe 5 buttons at a time with their own book objects
+            //next page, back page, and current page components
+            //big label displaying book button that was clicked on
+            //search component at top
+            //2 menus: add book and filter
+                //add book could bring up dialog box to put in book info?
+                //filter could be a drop-down with genres and ratings
+                    //genres could sort all available genres
+                    //ratings could sort most liked to least liked
     }
 }
